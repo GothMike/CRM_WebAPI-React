dad11d7dbfae2f1e4d0c07be60d39f0afab4484b
GothMike <117478074+GothMike@users.noreply.github.com>
2023-09-11 22:25:25 +0400

    Добавление в проект Factory method
    Создание интерфейса и реализация его для Factory method, который определяет какой репозиторий будет получен для указанного типа сущности.

---
+++ Persistence/Factory/Interfaces/IRepositoryFactory.cs
@@ -0,0 +1,17 @@
+﻿using CRM_WebAPI_React.Data.Repositories.Interfaces;
+
+namespace CRM_WebAPI_React.Persistence.Fabric.Interfaces
+{
+    /// <summary>
+    /// Интерфейс фабрики репозиториев, который определяет метод для получения репозитория для указанного типа сущности.
+    /// </summary>
+    public interface IRepositoryFactory
+    {
+        /// <summary>
+        /// Возвращает репозиторий указанного типа сущности.
+        /// </summary>
+        /// <typeparam name="T">Тип сущности, для которой нужен репозиторий.</typeparam>
+        /// <returns>Репозиторий указанного типа сущности.</returns>
+        IGenericRepository<T> GetRepository<T>() where T : class;
+    }
+}
